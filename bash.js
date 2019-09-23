376d1ab81d401816ad0d4d5e26cdfa80a91b3e74
Michael Mcmanus <mikemcmanus95@gmail.com>
2019-09-23 12:53:20 -0400

    Added ls command

--- bash.js
+++ bash.js
@@ -1,13 +1,16 @@
-const {pwd} = require('./pwd');
+const { pwd } = require('./pwd');
+const { ls } = require('./ls');
 
 process.stdout.write('prompt > ');
 
 process.stdin.on('data', data => {
   const cmd = data.toString().trim();
-  if(cmd === 'pwd'){
+  if (cmd === 'pwd') {
     pwd();
+  } else if (cmd === 'ls') {
+    ls();
   } else {
-  process.stdout.write('You typed: ' + cmd);
+    process.stdout.write('You typed: ' + cmd);
+    process.stdout.write('\nprompt > ');
   }
-  process.stdout.write('\nprompt > ');
 });
